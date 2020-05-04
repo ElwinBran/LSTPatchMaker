e43c8a19e1604e24d4de0505e3d920274200b12e
ElwinBran <elwinbran@gmail.com>
2020-05-04 10:15:55 +0200

    Updated imports to include only necessary classes.

--- src/main/java/com/gitlab/elwinbran/tools/lstpatchmaker/Program.java
+++ src/main/java/com/gitlab/elwinbran/tools/lstpatchmaker/Program.java
@@ -23,12 +23,19 @@
 package com.gitlab.elwinbran.tools.lstpatchmaker;
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
